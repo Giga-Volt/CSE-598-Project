f8a984c0803e59ed5e4880dab07076a69ed1b4e1
Gaurang Vichare <33225446+Giga-Volt@users.noreply.github.com>
2020-04-23 20:33:11 -0700

    Add files via upload

---
+++ initialize.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 14:49:54 2020
+
+@author: Gaurang Vichare
+"""
+
+from selenium import webdriver
+import win32gui
+import win32com.client
+import win32api
+import win32con
+import time
+
+def launchGame():
+    driver_path = 'C:\\Users/gvich/Downloads/chromedriver_win32/chromedriver'
+    browser = webdriver.Chrome(executable_path=driver_path)
+    browser.get('http://slither.io/')
+    
+    browser.set_window_size(900, 550)
+    
+    # Detect the game window
+    windows_list = []
+    toplist = []
+    def enum_win(hwnd, result):
+        win_text = win32gui.GetWindowText(hwnd)
+        windows_list.append((hwnd, win_text))
+    win32gui.EnumWindows(enum_win, toplist)
+    
+    # Game handle
+    game_hwnd = 0
+    for (hwnd, win_text) in windows_list:
+        if "slither.io" in win_text:
+            game_hwnd = hwnd
+    
+    # Set foreground
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.SendKeys('%')
+    win32gui.SetForegroundWindow(game_hwnd)
+    
+    input('Enter a name and change the snake to white. Then give an input:')
+    time.sleep(10)
+    win32api.SendMessage(game_hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN)
+    win32api.SendMessage(game_hwnd, win32con.WM_KEYUP, win32con.VK_RETURN)
+    
+    return game_hwnd
---
+++ test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 19:48:00 2020
+
+@author: Gaurang Vichare
+"""
+
+import win32gui
+import win32api
+import win32con
+import numpy as np
+import cv2
+from PIL import ImageGrab
+import time
+from initialize import launchGame
+
+def process_image(raw):
+    processed_image = cv2.cvtColor(raw, cv2.COLOR_RGB2GRAY)
+    (height, width) = processed_image.shape
+    processed_image = processed_image[111:height-8, 8:width-8]
+    (height, width) = processed_image.shape
+    if height < width:
+        difference = width - height
+        reduction = int(difference / 2)
+        processed_image = processed_image[:,reduction:width-reduction]
+    elif width < height:
+        difference = width - height
+        reduction = int(difference / 2)
+        processed_image = processed_image[reduction:height-reduction,:]
+    height = width = 50
+    dim = (height, width)
+    processed_image = cv2.resize(processed_image, dim, interpolation=cv2.INTER_LANCZOS4)
+    for i in range(height):
+        for j in range(width):
+            value = processed_image[i][j]
+            if value <= 63:
+                processed_image[i][j] = 0
+            elif value <= 127:
+                processed_image[i][j] = 96
+            elif value <= 191:
+                processed_image[i][j] = 160
+            else:
+                processed_image[i][j] = 255
+    return processed_image
+
+game_hwnd = launchGame();
+
+while True:
+    position = win32gui.GetWindowRect(game_hwnd)
+    # Take screenshot and process the image
+    raw = ImageGrab.grab(position)
+    raw = np.array(raw)
+    processed_image = process_image(raw)
+    
+    dim = (408, 408)
+    processed_image = cv2.resize(processed_image, dim, interpolation=cv2.INTER_NEAREST)
+    
+    cv2.imshow("Processed Image", processed_image)
+    
+    # Simulate keyboard input
+    win32api.SendMessage(game_hwnd, win32con.WM_KEYDOWN, win32con.VK_UP)
+    time.sleep(0.1)
+    win32api.SendMessage(game_hwnd, win32con.WM_KEYUP, win32con.VK_UP)
+    
+    key = cv2.waitKey(25)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
