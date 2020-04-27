1074b2ca6d6dcb75473675999e8b7ab6d0d32234
Gaurang Vichare <33225446+Giga-Volt@users.noreply.github.com>
2020-04-27 00:06:23 -0700

    Add files via upload

---
+++ image_processor.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 24 21:35:35 2020
+
+@author: Gaurang Vichare
+"""
+
+import cv2
+
+def process_image(raw):
+    processed_image = cv2.cvtColor(raw, cv2.COLOR_RGB2GRAY)
+    (height, width) = processed_image.shape
+    processed_image = processed_image[124:height-8, 8:width-8]
+    (height, width) = processed_image.shape
+    if height < width:
+        difference = width - height
+        reduction = int(difference / 2)
+        processed_image = processed_image[:,reduction:width-reduction]
+    elif width < height:
+        difference = width - height
+        reduction = int(difference / 2)
+        processed_image = processed_image[reduction:height-reduction,:]
+    height = width = 50
+    dim = (height, width)
+    processed_image = cv2.resize(processed_image, dim, interpolation=cv2.INTER_LANCZOS4)
+    for i in range(height):
+        for j in range(width):
+            value = processed_image[i][j]
+            if value <= 63:
+                processed_image[i][j] = 0
+            elif value <= 127:
+                processed_image[i][j] = 96
+            elif value <= 191:
+                processed_image[i][j] = 160
+            else:
+                processed_image[i][j] = 255
+    return processed_image
